f6d2eef29294d1702f169fbb750807158b1be53d
leonardopozzan <leonardo.lp.pozzan@gmail.com>
2022-10-21 21:05:43 +0200

    carosello infinito

--- js/libreria.js
+++ js/libreria.js
@@ -5,3 +5,52 @@ function addElementClassHTML(tagElement, className, fatherElement){
     fatherElement.append(tag);
     return tag;
 }
+
+//funzione che crea le immagini piccole del carosello
+function createMainImage(object){
+    const mainImage = document.createElement('div');
+    mainImage.className = 'col-12 main-image d-none';
+    mainImage.innerHTML = 
+    `
+    <img src="${object.url}" alt="${object.title}" class="big-dimension">
+    <div class="title-image">
+        <h1>${object.title}</h1>
+        <span class="description-image">${object.description}</span>
+    </div>
+    `
+    return mainImage;
+}
+
+//funzione che crea l'immagine grande
+function createCarouselImage(object){
+    const carouselImage = document.createElement('div');
+    carouselImage.className = 'col my-carousel-image my-opacity';
+    carouselImage.innerHTML = `<img src="${object.url}" alt="${object.title}" class="small-dimension">`
+    return carouselImage;
+}
+
+//funzione che crea l'intero caroselo
+function createCarousel(objectArray){
+    const myFlex = document.createElement('div');
+    myFlex.className = 'my-flex';
+    contaier.appendChild(myFlex);
+
+    for (let object of objectArray){
+        const mainImage = createMainImage(object);
+        myFlex.appendChild(mainImage);
+    }
+    for (let object of objectArray){
+        const carouselImage = createCarouselImage(object);
+        myFlex.appendChild(carouselImage);
+    }
+
+    myFlex.innerHTML += 
+    `
+    <input type="button" id="previous" class="btn btn-dark" value="Previous">
+    <input type="button" id="next" class="btn btn-dark" value="Next"></input>
+    `
+}
+
+function mod5(numb){
+    return numb%5
+}
